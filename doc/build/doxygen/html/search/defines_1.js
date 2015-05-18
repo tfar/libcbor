852e0184cbe30973abaa52e4229eda9fb73a6b12
PJK <me@pavelkalvoda.com>
2015-05-18 11:47:31 +0200

    Update generator, commit doxy

---
+++ doc/build/doxygen/html/search/defines_1.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['cbor_5fdummy_5fcallback',['CBOR_DUMMY_CALLBACK',['../callbacks_8c.html#a600badad1d123dd9fb8d74d5f2a29a5e',1,'callbacks.c']]],
+  ['cbor_5fhex_5fversion',['CBOR_HEX_VERSION',['../common_8h.html#ac61efb646fdd1936ac0ce32829f947b8',1,'common.h']]],
+  ['cbor_5fmajor_5fversion',['CBOR_MAJOR_VERSION',['../common_8h.html#a87cea1eae78853449c65c11d87517835',1,'common.h']]],
+  ['cbor_5fminor_5fversion',['CBOR_MINOR_VERSION',['../common_8h.html#a51fa4aa55e8743a687862f87ba574069',1,'common.h']]],
+  ['cbor_5fpatch_5fversion',['CBOR_PATCH_VERSION',['../common_8h.html#af2c19f68c53e5fa55957cbf1e65e1448',1,'common.h']]],
+  ['cbor_5fversion',['CBOR_VERSION',['../common_8h.html#a956e417f98370dd1a96e4dac2e0ed7dd',1,'common.h']]],
+  ['check_5fres',['CHECK_RES',['../builder__callbacks_8c.html#ac5a5fe93758a82606342a6f5513d1102',1,'builder_callbacks.c']]]
+];
---
+++ doc/build/doxygen/html/search/typedefs_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['_5fcbor_5ffree_5ft',['_cbor_free_t',['../common_8h.html#a39d0619c44bc7b3565a3c435e191cb14',1,'common.h']]],
+  ['_5fcbor_5fmalloc_5ft',['_cbor_malloc_t',['../common_8h.html#ad43a5d91c73c87d1a978273286e7c2be',1,'common.h']]],
+  ['_5fcbor_5frealloc_5ft',['_cbor_realloc_t',['../common_8h.html#a85a71755bd7dc24658b1769f77636758',1,'common.h']]]
+];
